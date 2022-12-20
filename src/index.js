5f4fcb2fcadbd26b36f48bfa423d2dbe09663eac
devingdaniels <devingdaniels@gmail.com>
2022-12-20 09:47:39 -0800

    Added static HTML files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log('Hello from index.js')
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+
+const PORT = process.env.PORT || 8080
+
+// Create a server
+const server = http.createServer((req, res) => { 
+    if (req.url === '/') { 
+        fs.readFile('public/index.html', (err, content) => { 
+            if (err) {
+                console.log(err)
+                throw new Error(err)
+            } else {
+                // Set the headers
+                res.writeHead(200, { 'Content-type': 'text/html' })
+                // Send back the content
+                res.end(content)
+             }
+        })
+    }
+})
+
+
+
+
+server.listen(PORT, ()=> console.log(`Listening on port --> ${PORT}`))
